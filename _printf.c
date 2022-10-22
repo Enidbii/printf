dcf7d70ddc79d9025ed2391d31734341cd975719
Enidbii <chebsenid@gmail.com>
2022-10-22 05:49:37 -0700

    printf

--- _printf.c
+++ _printf.c
@@ -1,7 +1,4 @@
-#include <stdio.h>
-#include <stdarg.h>
 #include "main.h"
-
 /**
  * _printf - produces output according to a format
  * @format: character string
@@ -9,18 +6,16 @@
  */
 int _printf(const char *format, ...)
 {
-	unsigned int i = 0;
-	unsigned int length = 0;
-	unsigned int index_c = 0;
+	unsigned int i = 0, length = 0, index_c = 0;
 	int (*function)(va_list, char *, unsigned int);
 	va_list args;
 	char *char_c;
 
 	va_start(args, format), char_c = malloc(sizeof(char) * 1024);
-	if (!char_c || !format || (format[i] == '%' && !format[i + 1]))
-		return (-1);
+	if (!char_c || !format || (format[i] == '%' && !format[i + 1])
+			return (-1);
 	if (!format[i])
-		return (0);
+	return (0);
 	for (i = 0; format && format[i]; i++)
 	{
 		if (format[i] == '%')
@@ -32,18 +27,18 @@ int _printf(const char *format, ...)
 			}
 			else
 			{
-				function = print_function(format, i + 1)
-					if (function == NULL)
-					{
-						if (format[i + 1] == ' ' && !format[i + 2])
-							return (-1);
-						charc_handler(char_c, format[i], index_c), length++, i--;
-					}
-					else
-					{
-						length = length + function(args, char_c, index_c);
-						i = i + all_print_functions(format, i + 1);
-					}
+				function = print_function(format, i + 1);
+				if (function == NULL)
+				{
+					if (format[i + 1] == ' ' && !format[i + 2])
+					return (-1);
+					charc_handler(char_c, format[i], index_c), length++, i--;
+				}
+				else
+				{
+					length = length + function(args, char_c, index_c);
+					i = i + all_print_functions(format, i + 1);
+				}
 			}
 			i++;
 		}
